a869d02b6d28aaeff41b359e4437a63c92347efe
gmp26 <gmp26@cam.ac.uk>
2016-09-05 15:08:01 +0100

    working js version in shiny

---
+++ js/tangleTest/ui.R
@@ -0,0 +1,51 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  
+  # Application title
+  titlePanel("Tangle Test (Javascript version)"),
+  
+  # Sidebar with a slider input for number of bins
+
+  sidebarLayout(
+    sidebarPanel(
+      
+      h4("A standard slider"),
+      
+      sliderInput("sbins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("sdistPlot")
+    )
+  ),
+  sidebarLayout(
+    sidebarPanel(
+      
+      h4("A tangle is an inline slider"),
+      
+      p("The histogram uses ",
+        tanglekit::tk_drag(30, " bins"),
+        ". Try dragging that number to the right or left.")
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
